7d4ecfa0ad3fd0f59b48cf5068a49493a78ddf94
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-03-30 02:13:04 -0500

    Time: 299 ms (90.53%), Space: 20 MB (37.63%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        dictVal = defaultdict(int)
+        out = 0
+        l1 = 0
+        l2 = 0
+        for r in range(len(nums)):
+            dictVal[nums[r]] += 1
+            
+            while len(dictVal) > k:
+                dictVal[nums[l2]] -=1
+                if dictVal[nums[l2]] == 0:
+                    dictVal.pop(nums[l2])
+                l2+=1
+                l1=l2
+                
+            while dictVal[nums[l2]] > 1:
+                dictVal[nums[l2]] -=1
+                l2+=1
+                
+            if len(dictVal) == k:
+                out += l2-l1+1
+        return out 
+        
