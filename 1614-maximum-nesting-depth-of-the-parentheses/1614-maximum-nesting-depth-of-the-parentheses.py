9f9211f298010a2aa346dd06ce2ab352a645f0d8
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-04-03 22:29:36 -0500

    Time: 41 ms (16.56%), Space: 16.4 MB (90.66%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -5,8 +5,8 @@ class Solution:
         for i in s:
             if i == "(":
                 count+=1
-            out = max(out, count)
-            if i == ")":
+            elif i == ")":
                 count-=1
+            out = max(out, count)
         return out
         
