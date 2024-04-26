d1e3d919a28aa429940c01c4947311673cfd2c20
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-04-26 17:42:30 -0500

    Time: 415 ms (49.78%), Space: 20.1 MB (63.88%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -1,15 +1,31 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        dp = grid[0]
         
+        def getMinTwo(row):
+            minVals = []
+            for i, v in row:
+                if len(minVals) < 2:
+                    minVals.append((i, v))
+                elif minVals[1][1] > v:
+                    minVals.pop()
+                    minVals.append((i, v))
+                minVals.sort(key= lambda x: x[1])
+            return minVals
+            
+            
+        n = len(grid)
+        firstRow = [(i, v) for i, v in enumerate(grid[0])] 
+        dp = getMinTwo(firstRow)
         for i in range(1, n):
-            x = float("inf")
-            nxt_dp = [x] * n
+            nxt_dp = []
             for p in range(n):
-                for q in range(n):
-                    if p != q:
-                        nxt_dp[p] = min(nxt_dp[p], grid[i][p]+dp[q]) 
+                curVal = grid[i][p]
+                minVal = float("inf")
+                for idx, val in dp:
+                    if p != idx:
+                        minVal = min(minVal,  curVal + val) 
+                nxt_dp.append((p, minVal))
+                nxt_dp = getMinTwo(nxt_dp)
             dp = nxt_dp
-        return min(dp)
+        return min([val for i, val in dp])
         
