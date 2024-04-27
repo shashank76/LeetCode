0a9aa84c098fb8e1f126e69d4f92c2eb01df9fe3
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-04-27 01:14:57 -0500

    Time: 2424 ms (6.27%), Space: 16.6 MB (76.47%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.py
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -1,22 +1,36 @@
 class Solution:
     def findRotateSteps(self, ring: str, key: str) -> int:
-        cache = {}
+        dp = [0] * len(ring)
         
-        def findMinTrail(k, v):
-            if v == len(key):
-                return 0
-            if (k, v) in cache:
-                return cache[(k, v)]
+        for v in reversed(range(len(key))):
+            new_dp = [float("inf")] * len(ring)
+            for k in range(len(ring)):
+                for i, val in enumerate(ring):
+                    if val == key[v]:
+                        minDist = min(abs(k-i), len(ring) - abs(k-i))
+                        new_dp[k] = min(new_dp[k], minDist + 1 + dp[i])
+            dp = new_dp
+        return dp[0]
+                
+                
+                
+#         cache = {}
+        
+#         def findMinTrail(k, v):
+#             if v == len(key):
+#                 return 0
+#             if (k, v) in cache:
+#                 return cache[(k, v)]
             
-            out = float('inf')
-            for i, val in enumerate(ring):
-                if val == key[v]:
-                    minDist = min(abs(k-i), len(ring) - abs(k-i))
-                    out = min(out, minDist+1+findMinTrail(i, v+1))
-            cache[(k, v)] = out
-            return out
+#             out = float('inf')
+#             for i, val in enumerate(ring):
+#                 if val == key[v]:
+#                     minDist = min(abs(k-i), len(ring) - abs(k-i))
+#                     out = min(out, minDist+1+findMinTrail(i, v+1))
+#             cache[(k, v)] = out
+#             return out
         
-        return findMinTrail(0, 0)
+#         return findMinTrail(0, 0)
                 
                     
             
