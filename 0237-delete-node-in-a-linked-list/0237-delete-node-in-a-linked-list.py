63e7dacb319f7ff6a7996e349b4a2d559ff46f78
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-05-04 21:54:29 -0500

    Time: 46 ms (8.94%), Space: 16.8 MB (70.90%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -6,10 +6,6 @@
 
 class Solution:
     def deleteNode(self, node):
-        """
-        :type node: ListNode
-        :rtype: void Do not return anything, modify node in-place instead.
-        """
         temp = node
         node.val = temp.next.val
         node.next = temp.next.next
