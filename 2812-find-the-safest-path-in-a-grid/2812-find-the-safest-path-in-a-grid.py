c948384c4ce6e7111aecdda8511622367bd6d614
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-05-17 19:33:33 -0500

    Time: 5730 ms (12.72%), Space: 69.9 MB (8.32%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
@@ -1,73 +1,41 @@
 class Solution:
     def maximumSafenessFactor(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        queue = collections.deque()
-        new_grid = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
+        
+        def isValidIndex(r, c):
+            return min(r, c) >= 0 and max(r,c) < n
+        
+        def calculateDist():
+            q = collections.deque()
+            min_dist={}
+            
+            for i in range(n):
+                for j in range(n):
+                    if grid[i][j]:
+                        q.append([i,j,0])
+                        min_dist[i, j] = 0           
+            while q:
+                i, j, dist = q.popleft()
+                adj_vals = [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]
+                for x, y in adj_vals:
+                    if isValidIndex(x, y) and (x, y) not in min_dist:
+                        min_dist[x, y] = dist+1
+                        q.append([x, y, dist+1])
+            return min_dist
+        
+        min_dist = calculateDist()
+        maxHeap = [(-min_dist[(0,0)], 0, 0)]
         visited = set()
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    queue.append([i,j,0])
-                    visited.add((i,j))
-        ans = 0
-        
-        while len(queue)>0:
-            i,j,dist = queue.popleft()
-            new_grid[i][j] = dist
-            
-            if i+1 < n and (i+1,j) not in visited and grid[i+1][j] == 0:
-                visited.add((i+1,j))
-                queue.append([i+1,j,dist+1])
-            
-            if i-1 >= 0 and (i-1,j) not in visited and grid[i-1][j] == 0:
-                visited.add((i-1,j))
-                queue.append([i-1,j,dist+1])
-            
-            if j-1 >= 0 and (i,j-1) not in visited and grid[i][j-1] == 0:
-                visited.add((i,j-1))
-                queue.append([i,j-1,dist+1])
-            
-            if j+1 < n and (i,j+1) not in visited and grid[i][j+1] == 0:
-                visited.add((i,j+1))
-                queue.append([i,j+1,dist+1])
-        
-        
-        def isSafe(val):
-            queue = collections.deque()
-            queue.append([0,0])
-            visited = set()
-            visited.add((0,0))
-            if new_grid[0][0] < val:
-                return False
-            while len(queue)>0:
-                i,j = queue.popleft()
-                if (i,j) == (n-1,n-1):
-                    return True
-                if i +1 < n and new_grid[i+1][j] >= val and (i+1,j) not in visited:
-                    queue.append([i+1,j])
-                    visited.add((i+1,j))
-                if i -1 >= 0 and new_grid[i-1][j] >= val and (i-1,j) not in visited:
-                    queue.append([i-1,j])
-                    visited.add((i-1,j))
-                
-                if j +1 < n and new_grid[i][j+1] >= val and (i,j+1) not in visited:
-                    queue.append([i,j+1])
-                    visited.add((i,j+1))
-                
-                if j - 1 >= 0 and new_grid[i][j-1] >= val and (i,j-1) not in visited:
-                    queue.append([i,j-1])
-                    visited.add((i,j-1))
-            
-            return False
-        
-        l,h,ans = 0,10**3,-1
-        while l <= h:
-            mid = (l+h)//2 
-            if isSafe(mid):
-                ans = mid
-                l = mid + 1
-            else:
-                h = mid -1
-        
-        return ans
-        
+        visited.add((0,0))
+        while maxHeap:
+            dist, r, c = heapq.heappop(maxHeap)
+            dist = -dist
+            if (r, c) == (n-1,n-1):
+                return dist
+
+            adj_vals = [[r+1, c], [r-1, c], [r, c+1], [r, c-1]]
+            for x, y in adj_vals:
+                if isValidIndex(x, y) and (x, y) not in visited:
+                    visited.add((x, y))
+                    new_dist = min(dist, min_dist[x, y])
+                    heapq.heappush(maxHeap, (-new_dist, x, y))
