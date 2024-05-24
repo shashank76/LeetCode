8a109b042b98096572532b8e0d3ed3248727b42d
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-05-23 19:10:03 -0500

    Time: 55 ms (89.97%), Space: 16.7 MB (53.18%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -1,17 +1,50 @@
 class Solution:
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
-        
-        def helper(i, count):
-            if i == len(nums):
+        counter = Counter(nums)
+        groups = []
+        visited=set()
+        cache={}
+        def helper(n, g):
+            if n not in g:
                 return 1
+            if n in cache:
+                return cache[n]
+            skip = helper(n+k, g)
+            include = (2**g[n]-1) * helper(n+2*k, g)
+            cache[n] = skip + include
+            return skip + include
             
-            out = helper(i+1, count)
-            
-            if not count[nums[i] + k] and not count[nums[i] -k]:
-                count[nums[i]] += 1
-                out += helper(i+1, count)
-                count[nums[i]] -= 1
-            return out
+        for n in counter.keys():
+            if n in visited:
+                continue
+            grp={}
+            while n-k in counter:
+                n-=k
+            while n in counter:
+                grp[n] = counter[n]
+                visited.add(n)
+                n += k
+            groups.append(grp)
         
-        return helper(0, defaultdict(int)) - 1
+        out = 1
+        for grp in groups:
+            n = min(grp.keys())
+            out *= helper(n, grp)
+        return out -1
+            
+            
+        
+#         def helper(i, count):
+#             if i == len(nums):
+#                 return 1
+            
+#             out = helper(i+1, count)
+            
+#             if not count[nums[i] + k] and not count[nums[i] -k]:
+#                 count[nums[i]] += 1
+#                 out += helper(i+1, count)
+#                 count[nums[i]] -= 1
+#             return out
+        
+#         return helper(0, defaultdict(int)) - 1
         
