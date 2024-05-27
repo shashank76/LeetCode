7a469df74f0850eaf3af7ef72950646fa53daf78
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-05-26 21:57:56 -0500

    Time: 46 ms (28.68%), Space: 16.5 MB (48.53%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -0,0 +1,16 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        prev = 0
+        i = 0
+        print(nums)
+        while i < len(nums):
+            if nums[i] < n:
+                n-=1
+            elif nums[i] >= n and prev < n:
+                return n
+            prev = nums[i]
+            i+=1
+        return -1
+        
