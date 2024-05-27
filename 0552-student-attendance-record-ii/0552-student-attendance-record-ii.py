c4aa7525f91f4cb694bc94dee0dd004d77e2b06e
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-05-26 20:09:35 -0500

    Time: 1591 ms (60.98%), Space: 16.5 MB (89.76%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.py
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.py
@@ -1,20 +1,25 @@
 class Solution:
     def checkRecord(self, n: int) -> int:
-        @cache
-        
-        def helper(i, j, k):
-            if i >= n:
-                return 1
-            out = 0
-            if j == 0:
-                out += helper(i + 1, j + 1, 0)
-            if k < 2:
-                out += helper(i + 1, j, k + 1)
-            out += helper(i + 1, j, 0)
-            return out % mod
-
         mod = 10**9 + 7
-        out = helper(0, 0, 0)
-        helper.cache_clear()
-        return out
+        if n == 1:
+            return 3
+        
+        temp = { (0,0): 1, (0,1): 1, (0,2): 0, (1,0): 1, (1,1): 0, (1,2): 0 }
+        for i in range(n-1):
+            out = defaultdict(int)
+            
+            out[(0,0)] = ((temp[(0,0)] + temp[(0,1)]) % mod + temp[(0,2)]) % mod
+            out[(1,0)] = ((temp[(1,0)] + temp[(1,1)]) % mod + temp[(1,2)]) % mod
+            
+            out[(0,1)] = temp[(0,0)]
+            out[(1,1)] = temp[(1,0)]
+            out[(0,2)] = temp[(0,1)]
+            out[(1,2)] = temp[(1,1)]
+            
+            out[(1,0)] = (out[(1,0)] + ((temp[(0,0)] + temp[(0,1)]) % mod + temp[(0,2)]) % mod) % mod
+            
+            temp = out
+            
+        return sum(temp.values()) % mod
+            
         
