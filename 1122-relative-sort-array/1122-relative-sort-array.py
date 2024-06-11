6149c434090ee02dc72ce0494f9ddf410667020a
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-06-10 22:09:54 -0500

    Time: 31 ms (95.54%), Space: 16.7 MB (30.54%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -4,11 +4,11 @@ class Solution:
         remaining = sorted(list(counter.keys()))
         out = []
         for i in arr2:
-            for j in range(counter[i]):
+            for _ in range(counter[i]):
                 out.append(i)
             remaining.remove(i) 
         for i in remaining:
-            for j in range(counter[i]):
+            for _ in range(counter[i]):
                 out.append(i)
         return out
         
