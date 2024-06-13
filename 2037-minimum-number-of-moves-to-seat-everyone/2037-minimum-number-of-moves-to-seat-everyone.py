e02e6326b0e486c5f91d1a38ebe0f19740f62d94
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-06-13 15:18:05 -0500

    Time: 80 ms (6.76%), Space: 16.6 MB (9.30%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
+        seats.sort()
+        students.sort()
+        i = 0
+        out = 0
+        
+        def pos_arr(a, b):
+            count = 0
+            while a < b:
+                a +=1
+                count +=1
+            return count
+            
+        while i < len(seats):
+            if seats[i] < students[i]:
+                out += pos_arr(seats[i], students[i])
+            elif seats[i] > students[i]:
+                out += pos_arr(students[i], seats[i])
+            print(seats[i], students[i], out)
+            i+=1
+        return out
+        
