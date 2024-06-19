168fff62c4024bb2e220bc43a8054dff8fce59a2
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-06-19 12:43:32 -0500

    Time: 762 ms (79.62%), Space: 30.5 MB (46.85%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,41 +1,30 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        # bloomDay.sort()
-        # target = m*k
-        # total = 0
-        # i = 0
-        # while i < len(bloomDay) -1:
-        #     total = bloomDay[i] + bloomDay[i+1]
-        #     if bloomDay[i] % target == 0:
-        #         return bloomDay[i]
-        #     elif total % target == 0:
-        #         return total
-        #     i+=1
-        # return -1
-        
-        if m*k > len(bloomDay):
+        if m * k > len(bloomDay):
             return -1
-        
+
+        def checkBloomDays(days):
+            bouquets = 0
+            count = 0
+
+            for d in bloomDay:
+                if d > days:
+                    count = 0
+                else:
+                    count += 1
+                    if count == k:
+                        bouquets += 1
+                        count = 0
+
+            return bouquets >= m
+
         i, j = 1, max(bloomDay)
-        
+
         while i < j:
             mid = (i+j) // 2
-            cur = 0
-            total = 0
-            for day in bloomDay:
-                
-                if day > mid:
-                    cur = 0 
-                else:
-                    cur += 1
-                    
-                if cur >= k:
-                    cur = 0
-                    total += 1
-                if total == m:
-                    break
-            if total == m:
+            if checkBloomDays(mid):
                 j = mid
             else:
-                i = mid+1
+                i = mid + 1
+
         return i
