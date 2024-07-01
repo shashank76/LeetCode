b6cbc891dd7c870f2dd6f403f35a6e6ed70811be
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-07-01 15:41:41 -0500

    Time: 26 ms (96.56%), Space: 16.5 MB (45.54%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,4 +1,22 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        # return x**n
+        if n >= 0:
+            return self.power(x, n)
+        else:
+            return 1 / self.power(x, -n)
+
+    def power(self, i, n):
+        if n == 0:
+            return 1
+
+        temp = self.power(i, n // 2)
+
+        if n % 2 == 0:
+            return temp * temp
+        else:
+            if n > 0:
+                return i * temp * temp
+            else:
+                return (temp * temp) / i    
         
