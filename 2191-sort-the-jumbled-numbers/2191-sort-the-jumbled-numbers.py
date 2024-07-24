270aa91c4facc6b66bbf7a5de310b8f5e1715837
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-07-24 14:37:56 -0500

    Time: 1008 ms (67.50%), Space: 24.7 MB (61.50%) - LeetHub

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        pairVals = []
+        
+        for i, v in enumerate(nums):
+            val = str(v)
+            mappedVal =0
+            for j in val:
+                mappedVal *= 10
+                mappedVal += mapping[int(j)]
+            pairVals.append((mappedVal, i))
+        
+        pairVals.sort()
+        out = [nums[x[1]] for x in pairVals]
+        return out
