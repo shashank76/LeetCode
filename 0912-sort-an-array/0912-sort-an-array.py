daed31b3299f1a3af1a4817966ebd74baebceba7
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-07-25 16:58:43 -0500

    Time: 1187 ms (32.24%), Space: 36.3 MB (7.34%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,36 +1,41 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         
-        def mergeSort(nums):
-            if len(nums) > 1:
-                mid = len(nums)//2
-                arr1 = nums[:mid]
-                arr2 = nums[mid:]
-                mergeSort(arr1)
-                mergeSort(arr2)
-                i, j, k = 0,0,0
-                
-                while i < len(arr1) and j < len(arr2):
-                    if arr1[i] < arr2[j]:
-                        nums[k] = arr1[i]
-                        i+=1
-                    else:
-                        nums[k]=arr2[j]
-                        j+=1
-                    k+=1
-                
-                while i < len(arr1):
+        def mergeSort(arr, l, m, r):
+            arr1 = arr[l:m+1]
+            arr2 = arr[m+1:r+1]
+            
+            i, j, k = 0,0,l
+            
+            while i < len(arr1) and j < len(arr2):
+                if arr1[i] <= arr2[j]:
                     nums[k] = arr1[i]
                     i+=1
-                    k+=1
-                
-                while j < len(arr2):
-                    nums[k] = arr2[j]
+                else:
+                    nums[k]=arr2[j]
                     j+=1
-                    k+=1
-                    
-        mergeSort(nums)
-        return nums
+                k+=1
+           
+            while i < len(arr1):
+                nums[k] = arr1[i]
+                i+=1
+                k+=1
+
+            while j < len(arr2):
+                nums[k] = arr2[j]
+                j+=1
+                k+=1
+        
+        def devideArr(arr, i, j):
+            if i == j:
+                return arr
+            mid = (i+j) // 2
+            devideArr(arr, i, mid)
+            devideArr(arr, mid+1, j)    
+            mergeSort(arr, i, mid, j)
+            return arr
+        
+        return devideArr(nums, 0, len(nums)-1)
             
             
         
