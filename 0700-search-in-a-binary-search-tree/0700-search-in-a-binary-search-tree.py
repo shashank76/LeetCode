dd1196d04b13b43b91f10523a579fd6e2514da2d
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-08-07 20:49:39 -0500

    Time: 47 ms (93.34%), Space: 18.1 MB (99.16%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.py
@@ -9,10 +9,10 @@ class Solution:
         if not root or root.val == val:
             return root
         
-        if root.val > val:
-            return self.searchBST(root.left, val)
-        else:
+        if root.val < val:
             return self.searchBST(root.right, val)
+        else:
+            return self.searchBST(root.left, val)
             
         
         
