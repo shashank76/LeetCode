e9848c980bc74cb053c10672a01b0f17750d7a69
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-08-12 16:06:44 -0500

    Time: 76 ms (75.65%), Space: 20.4 MB (90.87%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.py
@@ -1,14 +1,17 @@
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.k = k
-        self.vals = nums      
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
         
 
     def add(self, val: int) -> int:
-        self.vals.append(val)
-        self.vals.sort()
-        return self.vals[-self.k]
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
         
 
 
