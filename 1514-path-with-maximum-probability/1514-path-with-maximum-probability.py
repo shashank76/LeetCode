0dec5f007f038b5ac4f493bcf5d73bf3e05af76b
Shashank Tripathi <shashank.tripathi0508@gmail.com>
2024-08-27 15:16:28 -0500

    Time: 548 ms (61.69%), Space: 28.7 MB (51.22%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        adjList = defaultdict(list)
+        for i in range(len(edges)):
+            a, b = edges[i]
+            adjList[a].append((b, succProb[i]))
+            adjList[b].append((a, succProb[i]))
+        
+        q = [(-1, start_node)]
+        visited = {}
+        while len(q):
+            val, node = heapq.heappop(q)
+            if node in visited:
+                continue
+            if node == end_node:
+                return -val
+            visited[node] = 1
+            for i, prob in adjList[node]:
+                heapq.heappush(q, (val*prob, i))
+        return 0
+        
